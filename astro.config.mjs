2a03314132e6aa420e3d7fd4eb59cfb1329a1930
João Lucas <jlucaso@hotmail.com>
2025-03-26 15:58:16 -0300

    Update HIDE_SOURCES to false in GitHub Actions workflow and switch to process.env for BASE_URL and SITE in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base: import.meta.env.BASE_URL,
-  site: import.meta.env.SITE,
+  base: process.env.BASE_URL,
+  site: process.env.SITE,
   vite: {
     build: {
       assetsInlineLimit: 0, // Ensure WebAssembly files aren't inlined

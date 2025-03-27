f9b3a2c28689a92e92ba830e113703e9998ce0d7
João Lucas <jlucaso@hotmail.com>
2025-03-27 16:47:01 -0300

    chore: added tailwind and adapt dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
@@ -10,8 +12,11 @@ export default defineConfig({
     build: {
       assetsInlineLimit: 0, // Ensure WebAssembly files aren't inlined
     },
+
     optimizeDeps: {
       exclude: ["fs", "path", "cheerio"], // Node.js modules used at build time
     },
+
+    plugins: [tailwindcss()],
   },
 });
